11c2c90399b33b6729ef1303395d9a6aedb0bff9
vassyan <vasa060504@mail.ru>
2023-11-20 16:43:04 +0300

    Entities done

---
+++ Bluff/Bluff.Domain/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluff.Domain
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int[]? Cubes { get; set; }
+
+        public int? CubesCount { get; set; }
+    }
+}
