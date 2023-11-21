e8e057124ec48e0c6b93c301230507a04fad7b1c
vassyan <vasa060504@mail.ru>
2023-11-21 19:21:16 +0300

    work on gamehub has started

---
+++ Bluff/Bluff.Server/Hubs/GameHub.cs
@@ -0,0 +1,19 @@
+﻿using Bluff.Domain;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Bluff.Server.Hubs
+{
+    public class GameHub : Hub
+    {
+        private List<Client> clients {  get; set; }
+        
+        private Client? betAuthor { get; set; }
+        
+        private Bet? bet {  get; set; }
+
+        public async Task UserConnected(string userName)
+        {
+            
+        }
+    }
+}
